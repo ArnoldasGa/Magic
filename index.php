23396be4fe27dcb3fa6b5ff77ed327ed63dfd9c0
unknown <arnoldasgaigalas@gmail.com>
2023-01-11 21:36:32 +0200

    update

--- index.php
+++ index.php
@@ -3,16 +3,13 @@
 include_once "Order.php";
 
 $magic = new Order("pending", 100, "aaa@gmail.com");
-$magic2 = new Order("pending", 100, "aaa@gmail.com");
-$magic3 = new Order("pending", 100, "aaa@gmail.com");
-
-// var_dump($magic);
+$magic2 = new Order("pending", 200, "aaa@gmail.com");
+$magic3 = new Order("in_progress", 400, "aaa@gmail.com");
 
 $newItem = clone $magic;
 $newItem2 = clone $magic2;
 $newItem3 = clone $magic3;
 
-// var_dump($magic);
 echo $magic . " Old item 1" . PHP_EOL;
 echo $magic2 . " Old item 2" . PHP_EOL;
 echo $magic3 . " Old item 3" . PHP_EOL;
